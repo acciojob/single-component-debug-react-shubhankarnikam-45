3e389bf8552ffa99f0930b5b95fba0c372d94b13
shubhankarnikam-45 <shubhankarnikam45@gmail.com>
2023-04-02 15:19:58 +0530

    2

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,11 @@ const App=()=>{
 
     return (
 
-        <div>
+      
             <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
 
-        </div>
+        
     )
 }
 
